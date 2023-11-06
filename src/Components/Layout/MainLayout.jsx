9a60fc0775f328ed4ddad9e2c92302340f0095da
AKD <contact.mousom@gmail.com>
2023-11-06 18:32:52 +0600

    Footer is added in mainLayout

--- src/Components/Layout/MainLayout.jsx
+++ src/Components/Layout/MainLayout.jsx
@@ -74,7 +74,7 @@ const MainLayout = ({ children }) => {
                     </ul>
                 </div>
             </div>
-
+            {/* footer section is added here */}
             <footer className="footer mt-4 footer-center p-4 bg-base-300 text-base-content">
                 <aside>
                     <p>Copyright © 2023 - All right reserved by Online Group Study </p>
