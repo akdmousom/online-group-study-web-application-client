4cb7ad0b6eceec4a947a4cc813476c1afc02d886
AKD <contact.mousom@gmail.com>
2023-11-07 01:51:38 +0600

    Now all form data store in one state it's helps send entire data in one state

--- src/Pages/CreateAssignments.jsx
+++ src/Pages/CreateAssignments.jsx
@@ -3,35 +3,57 @@ import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 const CreateAssignments = () => {
     const [startDate, setStartDate] = useState(new Date());
+        const [formdata,setFormData] = useState({
+        title: '',
+        description: '',
+        thumbnail: '',
+        difficultyLevel: '',
+        date: startDate,
+    })
+
+    const handleChenge = (e) =>{
+
+        const {name, value} = e.target
+        setFormData({
+            ...formdata,
+            [name]:value,
+        })
+
+    }
+
+    const handleSubmit = (e)=>{
+        e.preventDefault();
+        console.log(formdata);
+    }
 
     return (
         <div className="hero min-h-screen bg-base-200">
             <div className="hero-content flex-col lg:flex-row-reverse">
                 <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-                    <form className="card-body">
+                    <form onSubmit={handleSubmit} className="card-body">
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Title</span>
                             </label>
-                            <input type="text" placeholder="Title" className="input input-bordered" required />
+                            <input onChange={handleChenge} type="text" name="title" value={formdata.value} placeholder="Title" className="input input-bordered" required />
                         </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Description</span>
                             </label>
-                            <input type="text" placeholder="Description" className="input input-bordered" required />
+                            <input onChange={handleChenge} type="text" name="description" value={formdata.value} placeholder="Description" className="input input-bordered" required />
                         </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Thumbnail</span>
                             </label>
-                            <input type="text" placeholder="Thumbnail" className="input input-bordered" required />
+                            <input onChange={handleChenge} type="text" name="thumbnail" value={formdata.value} placeholder="Thumbnail" className="input input-bordered" required />
                         </div>
                         <div className="form-control">
                         <label className="label">
                                 <span className="label-text">Difficulty level</span>
                             </label>
-                            <select className="select select-bordered w-full max-w-xs">
+                            <select onChange={handleChenge} name="difficultyLevel" value={formdata.value} className="select select-bordered w-full max-w-xs">
                                 <option>Easy</option>
                                 <option>Medium</option>
                                 <option>Hard</option>
@@ -45,7 +67,7 @@ const CreateAssignments = () => {
                         </div>
 
                         <div className="form-control mt-6">
-                            <button className="btn btn-primary">Login</button>
+                            <button type="submit" className="btn btn-primary">Create Assignment</button>
                         </div>
                     </form>
                 </div>
